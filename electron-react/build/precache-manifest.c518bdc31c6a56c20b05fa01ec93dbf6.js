6256c9bf6273d015523a1aaaf4dd841cc48f03b2
cesc993 <47029240+cesc993@users.noreply.github.com>
2021-03-08 23:41:22 +0100

    Add files via upload

---
+++ electron-react/build/precache-manifest.c518bdc31c6a56c20b05fa01ec93dbf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd07672629eaccc5bfc17cee416d7dad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f2b3dbd7aaaab9162799",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8b29020b43f6dbcf0ddd",
+    "url": "./static/js/2.56d6615f.chunk.js"
+  },
+  {
+    "revision": "f2b3dbd7aaaab9162799",
+    "url": "./static/js/main.ff2a7980.chunk.js"
+  },
+  {
+    "revision": "1a2e64b3bab7e5288c23",
+    "url": "./static/js/runtime-main.e1982eea.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
